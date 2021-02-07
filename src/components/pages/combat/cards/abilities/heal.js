347e7c678f8c63f990fba46bdea00246843f0337
Richard <richardbrianhall@gmail.com>
2021-02-07 15:39:49 -0600

    heal sound

--- src/components/pages/combat/cards/abilities/heal.js
+++ src/components/pages/combat/cards/abilities/heal.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import "../cards.css"
+import {Howl, Howler} from "howler";
+import Healing from "../../../../assets/sounds/healing.wav";
+
+const Music = [{sound: Healing}]
 
 class Heal extends Component {
-    render() {
-        return(
-            <input type="button" className="icon" id="heal"></input>
+
+    Sound = (src) => {
+        const sound = new Howl ({
+            src})
+            sound.play();
+        
+    }
+
+    abilityandsound = () => {
+        return Music.map((soundObj, index) => {
+        return (
+            <button key={index} id="heal" onClick={() => this.Sound(soundObj.sound)}> 
+            </button>
         )
+        })
+    }
+    render() {
+        Howler.volume(4.0)
+        return <div className="sound">
+        {this.abilityandsound()}
+        </div>
     }
 }
+
 export default Heal
